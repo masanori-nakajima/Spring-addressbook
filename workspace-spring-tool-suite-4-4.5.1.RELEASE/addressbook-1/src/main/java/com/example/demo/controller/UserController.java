390d108521f2259d8faf0208bf07b1b8405dc325
masanori-nakajima <nakajima.masanori@gb-sense.com>
2020-03-11 18:04:19 +0900

    3月11日本日の作業ファイル

--- workspace-spring-tool-suite-4-4.5.1.RELEASE/addressbook-1/src/main/java/com/example/demo/controller/UserController.java
+++ workspace-spring-tool-suite-4-4.5.1.RELEASE/addressbook-1/src/main/java/com/example/demo/controller/UserController.java
@@ -8,6 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.UserRequestDto.UserRequest;
+import com.example.demo.controller.UserService.UserService;
 import com.example.demo.Repository.UserRepository;
 import com.example.demo.entity.User;
 
@@ -22,18 +25,15 @@ public class UserController {
 			model.addAttribute("user", userlist);
 			return "user";
 	}
-	
-	
-	@RequestMapping(value="/user/add", method= RequestMethod.GET)
-	public String displayAdd (Model model) {
+
+	@RequestMapping(value = "/user/add", method=RequestMethod.GET)
+	public String displayAdd(Model model) {
 			model.addAttribute("userRequest", new UserRequest());
 			return "user/add";
 	}
-	
-	@RequestMapping(value="/user/create", method= RequestMethod.POST)
-	public String create (@ModelAttribute UserRequest userRequest, Model model) {
-			userService.create(userRequest);
-			return "redirect:/user";
+	@RequestMapping(value="/user/create", method=RequestMethod.POST)
+	public String create(@ModelAttribute UserRequest userRequest, Model model) {
+				UserService.create(userRequest);
+				return "redirect:/user/list";
 	}
-	
 }
