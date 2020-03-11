bae42fc2799e82fc845dd095cbeb72810c23df4d
masanori-nakajima <nakajima.masanori@gb-sense.com>
2020-03-11 15:28:46 +0900

    住所録開発新規ファイル

---
+++ workspace-spring-tool-suite-4-4.5.1.RELEASE/addressbook-1/src/main/java/com/example/demo/Addressbook1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Addressbook1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Addressbook1Application.class, args);
+	}
+
+}
